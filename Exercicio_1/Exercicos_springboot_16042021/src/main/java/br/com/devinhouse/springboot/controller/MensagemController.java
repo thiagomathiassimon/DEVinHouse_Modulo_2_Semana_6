395d0bceabead638855e3fba9b3c55d89b1f3759
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-04-16 11:50:21 -0300

    Exercicio_1

---
+++ Exercicio_1/Exercicos_springboot_16042021/src/main/java/br/com/devinhouse/springboot/controller/MensagemController.java
@@ -0,0 +1,26 @@
+package br.com.devinhouse.springboot.controller;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.devinhouse.springboot.service.MensagemService;
+
+@RestController
+@RequestMapping(value = "/mensagem")
+public class MensagemController {
+
+	@Autowired
+	MensagemService service;
+	
+	@RequestMapping(value = "/v1" + "/imprimir", method = GET, produces = APPLICATION_JSON_VALUE)
+	@ResponseBody
+	public String imprimirMensagem(){
+		return service.olaMundo();
+	}
+	
+}
---
+++ Exercicio_1/Exercicos_springboot_16042021/src/main/java/br/com/devinhouse/springboot/service/MensagemService.java
@@ -0,0 +1,14 @@
+package br.com.devinhouse.springboot.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class MensagemService {
+
+	public String olaMundo() {
+		return "Olá Mundo!";
+	}
+
+
+	
+}
