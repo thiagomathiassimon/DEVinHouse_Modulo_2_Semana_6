3bceab10ebe93bdfc908e9bf07e976b428844e10
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-04-17 12:31:58 -0300

    Exercicio_8

---
+++ Exercicio_8/springbootapi-execercicios/src/main/java/br/com/devinhouse/springbootapiexercicios/service/ProcessoService.java
@@ -0,0 +1,47 @@
+package br.com.devinhouse.springbootapiexercicios.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.devinhouse.springbootapiexercicios.dto.ProcessoCriadoDTO;
+import br.com.devinhouse.springbootapiexercicios.dto.ProcessoDTO;
+import br.com.devinhouse.springbootapiexercicios.repository.ProcessoRepository;
+
+@Service
+public class ProcessoService {
+
+	@Autowired
+	private ProcessoRepository repository;
+
+	public List<ProcessoDTO> listarProcessos() {
+		return repository.findAllProcessos();
+	}
+
+	public List<ProcessoDTO> listarProcessosPorAno(String ano) {
+
+		List<ProcessoDTO> todosOsProcessos = repository.findAllProcessos();
+		List<ProcessoDTO> listaDeProcessos = new ArrayList<ProcessoDTO>();
+
+		for (ProcessoDTO processoDTO : todosOsProcessos) {
+			if (ano.equals(processoDTO.getNuAnoProcesso())) {
+				listaDeProcessos.add(processoDTO);
+			}
+		}
+
+		return listaDeProcessos;
+	}
+
+	public ProcessoCriadoDTO cadastrarProcesso(ProcessoDTO processo) {
+
+		List<ProcessoDTO> todosOsProcessos = repository.findAllProcessos();
+		
+		todosOsProcessos.add(processo);
+		
+		ProcessoCriadoDTO processoCriado = new ProcessoCriadoDTO();
+		processoCriado.setChaveProcesso(processo.getChaveProcesso());
+		return processoCriado;
+	}
+}
