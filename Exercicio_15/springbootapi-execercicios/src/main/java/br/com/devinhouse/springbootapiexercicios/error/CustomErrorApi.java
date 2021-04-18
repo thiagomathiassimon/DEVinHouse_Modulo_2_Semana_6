68fac8098995699e1177130fb3febfd115d02ae1
Thiago Mathias Simon <thiago_m_simon@estudante.sc.senai.br>
2021-04-18 16:24:08 -0300

    Exercicio_15

---
+++ Exercicio_15/springbootapi-execercicios/src/main/java/br/com/devinhouse/springbootapiexercicios/error/CustomErrorApi.java
@@ -0,0 +1,68 @@
+package br.com.devinhouse.springbootapiexercicios.error;
+
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class CustomErrorApi extends RuntimeException {
+	
+	private static final long serialVersionUID = 8592524336571914971L;
+	
+	private String message;
+	private String details;
+	private String hint;
+	private String nextActions;
+	private String support;
+
+	protected CustomErrorApi() {
+	}
+
+	public CustomErrorApi(String message, String details, String hint, String nextActions, String support) {
+		this.message = message;
+		this.details = details;
+		this.hint = hint;
+		this.nextActions = nextActions;
+		this.support = support;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public String getDetails() {
+		return details;
+	}
+
+	public void setDetails(String details) {
+		this.details = details;
+	}
+
+	public String getHint() {
+		return hint;
+	}
+
+	public void setHint(String hint) {
+		this.hint = hint;
+	}
+
+	public String getNextActions() {
+		return nextActions;
+	}
+
+	public void setNextActions(String nextActions) {
+		this.nextActions = nextActions;
+	}
+
+	public String getSupport() {
+		return support;
+	}
+
+	public void setSupport(String support) {
+		this.support = support;
+	}
+
+}
